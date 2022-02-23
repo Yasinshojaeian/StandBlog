7c5810a66904b45feb976dd4447586bb58e55203
yasinshojaeian <yasin-4831-@yasinshojaeians-MacBook-Pro.local>
2022-02-23 10:52:22 +0330

    first git

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+from django.utils import timezone
+
+class Category(models.Model):
+    title = models.CharField(max_length=60)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
 
 class Article(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE,)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=70)
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
+    pub_date = models.DateField(default=timezone.now())
     def __str__(self):
         return  f'{self.title} - {self.body[:30]}'
